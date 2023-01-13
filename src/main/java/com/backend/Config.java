c37744cf422ce6266dd6e339d93f7128f026e820
JohnnyDesk <absolutec123@gmail.com>
2023-01-14 01:08:53 +0530

    added some intial files

---
+++ src/main/java/com/backend/Config.java
@@ -0,0 +1,9 @@
+package com.backend;
+
+public class Config {
+	
+	public void m1() {
+		System.out.println("Hello from Config");
+	}
+
+}
---
+++ src/test/java/com/teststeps/Browse.java
@@ -0,0 +1,12 @@
+package com.teststeps;
+
+import org.testng.annotations.Test;
+
+public class Browse {
+	
+	@Test
+	public void m2() {
+		System.out.println("hello from test case class");
+	}
+
+}
